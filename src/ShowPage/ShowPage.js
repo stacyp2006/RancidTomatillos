750cdee0fd72e59953ce1415fb19e24c856afe0a
Tim Keresey <tim.keresey@gmail.com>
2020-10-10 11:03:33 -0600

    create ShowPage component

--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -1,2 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch, Link, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './ShowPage.css';
+
+class ShowPage extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      video: []
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render () {
+    return (
+      <section>
+        <h1>{}</h1>
+        <h2>{}</h2>
+        <h2>{}</h2>
+        <video>
+          <source src={} type={} />
+        </video>
+      </section>
+    )
+  }
+}
+
+export default ShowPage;
