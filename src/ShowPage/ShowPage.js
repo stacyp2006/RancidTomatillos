35876f37b1f56acf3cc3370439d9d8c94b37af2d
Tim Keresey <tim.keresey@gmail.com>
2020-10-10 11:58:08 -0600

    create fetch method for showpage component

--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
+import Card from '../Card/Card';
 import { Route, Switch, Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './ShowPage.css';
 
 class ShowPage extends Component {
   constructor() {
-    super();
+    super(props);
 
     this.state = {
       movie: {},
-      video: []
+      videos: []
     }
   }
 
   componentDidMount() {
-    
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props}`)
+      .then(response => response.json())
+      .then(data => this.setState({movie: data.movie}))
+      .catch(error => console.log('error'))
+
   }
 
   render () {
     return (
       <section>
-        <h1>{}</h1>
-        <h2>{}</h2>
-        <h2>{}</h2>
-        <video>
-          <source src={} type={} />
-        </video>
+        <h1>title</h1>
+        <h2>release date</h2>
+        <h2>avg rating</h2>
+        <Videos videosList={this.state.videos}/>
       </section>
     )
   }
