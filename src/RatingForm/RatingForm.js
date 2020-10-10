aa8c2de76349175ceef7e045cd2711a38d41e6f5
Tim Keresey <tim.keresey@gmail.com>
2020-10-10 16:05:29 -0600

    create RatingForm component

---
+++ src/RatingForm/RatingForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Route, Switch, Link, NavLink } from 'react-router-dom';
+import './RatingForm.css';
+
+class RatingForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userRating: ''
+    }
+  }
+
+  updateValue(event) {
+    this.setState({userRating: event.target.value});
+  }
+
+  render() {
+    <div>
+      <form>
+        <label>Rate This Movie</label>
+        <input
+        type='number'
+        min='1' max='10'
+        name='rating'
+        value=''
+        />
+        <input
+        type='submit'
+        onClick={this.updateValue}
+        />
+      </form>
+      <button>Delete Rating</button>
+    </div>
+  }
+}
--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card';
+import RatingForm from '../RatingForm/RatingForm';
 import { Route, Switch, Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './ShowPage.css';
@@ -10,19 +11,20 @@ class ShowPage extends Component {
 
     this.state = {
       movie: {},
-      videos: []
+      videos: [],
+      userRating: ''
     }
   }
 
   fetchMovieObj() {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props}`)
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieID}`)
       .then(response => response.json())
       .then(data => this.setState({movie: data.movie}))
       .catch(error => console.log('movie fetch error'))
   }
 
   fetchVideos() {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props}/videos`)
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieID}/videos`)
       .then(response => response.json())
       .then(data => this.setState({videos: data.videos}))
       .catch(error => console.log('video fetch error'))
@@ -49,12 +51,14 @@ class ShowPage extends Component {
           <h2>{film.tagline}</h2>
           <h2>{film.overview}</h2>
         </section>
+        <Videos allVideos={this.state.videos} />
         <section className='info-section'>
           <h2>{film.release_date}</h2>
           <h2>{film.budget}</h2>
           <h2>{film.revenue}</h2>
           <h2>{film.runtime}</h2>
           <h2>{film.average_rating}</h2>
+          <h2>User Rating: </h2>
           <ul>
             {this.getGenres()}
           </ul>
