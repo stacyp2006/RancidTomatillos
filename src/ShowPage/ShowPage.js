916952831a1e165eeb9dbe1f814263b846a81e61
Stacy Potten <stacylynnaz@gmail.com>
2020-10-14 09:43:44 -0600

    Add methods to show user rating on ShowPage

--- src/ShowPage/ShowPage.js
+++ src/ShowPage/ShowPage.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import RatingForm from '../RatingForm/RatingForm';
-import { Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './ShowPage.css';
-import { singleMovieFetch } from '../apiCalls.js';
+import { singleMovieFetch, getAllRatings } from '../apiCalls.js';
 
 class ShowPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       movie: {},
-      userRating: ''
+      userRating: {}
     }
+  }
+
+  getUserRating = (ratingList) => {
+    const userID = this.props.userInfo.id;
+    getAllRatings(userID)
+    .then(data => ratingList = data.ratings)
+    .then(data => this.setState({userRating: this.findMovieRating(ratingList)}))
+    .catch(error => this.setState({error: error.message}))
+  }
+
+  findMovieRating = (ratingList) => {
+    const rating = ratingList.find(rating => {
+      return this.state.movie.id === rating.movie_id
+    });
+    return rating;
   }
 
   componentDidMount() {
     singleMovieFetch(this.props.id)
       .then(data => this.setState({movie: data.movie}))
+      .then(data => this.getUserRating())
       .catch(error => console.log({error: error.message}))
   }
 
@@ -49,6 +64,9 @@ class ShowPage extends Component {
           <ul>
             <li>{film.genres}</li>
           </ul>
+        </section>
+        <section> {this.props.userInfo.id && <h2>User Rating: {this.state.userRating.rating}</h2>}</section>
+        <section> {this.props.userInfo.id && <button onClick={this.deleteRating}>Delete Rating</button>}
         </section>
         <section>
         {this.props.userInfo.id &&
