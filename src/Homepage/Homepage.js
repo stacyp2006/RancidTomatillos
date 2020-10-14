682b21cd6d8cd83397944723ffd84a3384c09916
Stacy Potten <stacylynnaz@gmail.com>
2020-10-14 13:53:57 -0600

    Clean up unnecessary code on Homepage.

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Movies from '../Movies/Movies.js';
-import ShowPage from '../ShowPage/ShowPage.js';
-import { Route, Switch, Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Homepage.css';
 
