36b6b69c11d1408c877532f7990e541d4b3251c5
Stacy Potten <stacylynnaz@gmail.com>
2020-10-18 15:37:16 -0600

    Add unit test for render of login page

--- src/Login/Login.test.js
+++ src/Login/Login.test.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Login', () => {
+  it('should render a login form', () => {
+    const fakeAddUser = jest.fn();
+    render (<BrowserRouter><Login addUser={fakeAddUser}/></BrowserRouter>);
+    expect(screen.getByText('Login Here')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter your email')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter your password')).toBeInTheDocument();
+    expect(screen.getByText('login')).toBeInTheDocument();
+  })
+})
 
 
 // it('should render with a logout link if a user is logged in', () => {
