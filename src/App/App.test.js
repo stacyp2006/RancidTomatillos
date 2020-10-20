9c42b79c8c2570a7e4cf16b0d79cbfc263395942
Tim Keresey <tim.keresey@gmail.com>
2020-10-20 04:19:55 -0600

    add App tests for home and login links

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,15 +2,18 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import Login from '../Login/Login.js';
 import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import { moviesFetch } from '../apiCalls.js'
 jest.mock('../apiCalls.js');
 // import '@testing-library/jest-dom';
-// import Homepage from '../Homepage/Homepage.js';
+import Homepage from '../Homepage/Homepage.js';
+import Movies from '../Movies/Movies.js';
 
 describe('App', () => {
   it('should render with a home link and a login link', () => {
+    moviesFetch.mockResolvedValueOnce([]);
     render(
       <BrowserRouter><App /></BrowserRouter>
     );
@@ -24,7 +27,7 @@ describe('App', () => {
 //execution/assertion: expect avg rating to be in the document
   it('should render displaying movies', async () => {
 
-  moviesFetch.mockResolvedValueOnce([
+  const fakeMovies =  moviesFetch.mockResolvedValueOnce([
     {
     id: 694919,
     poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
@@ -42,14 +45,24 @@ describe('App', () => {
     release_date: "2020-09-04"
     }
   ]);
-
+  const fakeHome = { movies: fakeMovies };
+  const fakeUserInfo = {
+    loggedIn: true,
+    id: 1,
+    name: 'Rick',
+    userRating: [1, 2]
+  }
   render(
-    <BrowserRouter><App/></BrowserRouter>
+    <BrowserRouter>
+      <App />
+      <Homepage home={fakeHome}/>
+      <Movies moviesList={fakeMovies} loggedIn={false} userInfo={fakeUserInfo}/>
+    </BrowserRouter>
   );
   // render(
   //   <BrowserRouter><Homepage /></BrowserRouter>?
   // );
-  const avgRating1 = await waitFor(() => screen.getByText('5'));
+  const avgRating1 = await waitFor( () => screen.getByText('Average Rating:'));
   expect(avgRating1).toBeInTheDocument();
   })
 
@@ -59,10 +72,23 @@ describe('App', () => {
 
 //when you click home it takes you home
 it('should return to home view when Home is clicked', () => {
-
+  moviesFetch.mockResolvedValueOnce([]);
   render(
-    <BrowserRouter><App/></BrowserRouter>
+    <BrowserRouter><App /></BrowserRouter>
   );
+
+  userEvent.click(screen.getByText('Home'));
+  expect(screen.getByText('Welcome, Movie Buff!'))
 })
 
 //when login button is clicked, the login page is rendered
+
+it('should render the login page when the login link is clicked', () => {
+  moviesFetch.mockResolvedValueOnce([]);
+  render(
+    <BrowserRouter><App /><Login /></BrowserRouter>
+  );
+
+  userEvent.click(screen.getByText('Home'));
+  expect(screen.getByText('Login Here'));
+})
